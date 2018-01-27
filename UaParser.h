75c342f8d0c419c95d6a26888a820732ee38e673
Robert Ayrapetyan <robert.ayrapetyan@gmail.com>
2018-01-26 23:43:10 -0800

    Back to 2-spaces ident

--- UaParser.h
+++ UaParser.h
@@ -5,66 +5,66 @@
 namespace uap_cpp {
 
 struct Generic {
-    Generic() : family("Other") {}
-    std::string family;
+  Generic() : family("Other") {}
+  std::string family;
 };
 
 struct Device : Generic {
-    std::string model;
-    std::string brand;
+  std::string model;
+  std::string brand;
 };
 
 struct Agent : Generic {
-    std::string major;
-    std::string minor;
-    std::string patch;
-    std::string patch_minor;
+  std::string major;
+  std::string minor;
+  std::string patch;
+  std::string patch_minor;
 
-    std::string toString() const {
-        return family + " " + toVersionString();
-    }
+  std::string toString() const {
+    return family + " " + toVersionString();
+  }
 
-    std::string toVersionString() const {
-        return (major.empty() ? "0" : major) + "." + (minor.empty() ? "0" : minor) + "." + (patch.empty() ? "0" : patch);
-    }
+  std::string toVersionString() const {
+    return (major.empty() ? "0" : major) + "." + (minor.empty() ? "0" : minor) + "." + (patch.empty() ? "0" : patch);
+  }
 };
 
 struct UserAgent {
-    Device device;
+  Device device;
 
-    Agent os;
-    Agent browser;
+  Agent os;
+  Agent browser;
 
-    std::string toFullString() const {
-        return browser.toString() + "/" + os.toString();
-    }
+  std::string toFullString() const {
+    return browser.toString() + "/" + os.toString();
+  }
 
-    bool isSpider() const {
-        return device.family == "Spider";
-    }
+  bool isSpider() const {
+    return device.family == "Spider";
+  }
 };
 
 enum DeviceType {
-    kUnknown = 0, kDesktop, kMobile, kTablet
+  kUnknown = 0, kDesktop, kMobile, kTablet
 };
 
 class UserAgentParser {
 public:
-    explicit UserAgentParser(const std::string& regexes_file_path);
+  explicit UserAgentParser(const std::string& regexes_file_path);
 
-    UserAgent parse(const std::string&) const;
+  UserAgent parse(const std::string&) const;
 
-    Device parse_device(const std::string&) const;
-    Agent parse_os(const std::string&) const;
-    Agent parse_browser(const std::string&) const;
-    
-    DeviceType device_type(const std::string&) const;
+  Device parse_device(const std::string&) const;
+  Agent parse_os(const std::string&) const;
+  Agent parse_browser(const std::string&) const;
+  
+  DeviceType device_type(const std::string&) const;
 
-    ~UserAgentParser();
+  ~UserAgentParser();
 
 private:
-    const std::string regexes_file_path_;
-    const void* ua_store_;
+  const std::string regexes_file_path_;
+  const void* ua_store_;
 };
 
 }

e1bb47ff02475065156bd1f6687b27259045d3ef
masklinn <github.com@masklinn.net>
2024-06-19 20:59:04 +0200

    Basic flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "C++ implementation of ua-parser";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            re2
+            libyamlcpp
+          ];
+        };
+      };
+    };
+}
